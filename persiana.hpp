a1d3db9e07896cf9cf83623fc51e875073e80c46
Bruno <contato.brunoluiz101@gmail.com>
2023-05-18 13:43:43 -0300

    Texture on persiana

--- persiana.hpp
+++ persiana.hpp
@@ -5,12 +5,14 @@
 #include "primitivas.hpp"
 #include "cores.hpp"
 #include "matematica.hpp"
+#include "SOIL.h"
 
 // Persiana que fica nas janelas do laboratório
 class Persiana{
     private: 
     float largura, altura, profundidade, larguraLamina, alturaSuporte;
     int quantidadadeLaminas;
+    GLuint persianaTextureID;
 
     // Valor do angulo que a persiana deve se mexer
     float anguloPersiana = 60;
@@ -30,6 +32,7 @@ class Persiana{
             quantidadadeLaminas = qLam;
             corSuporte = corS;
             corLamina = corL;
+            
         }
 
         void desenhar(){
@@ -42,7 +45,27 @@ class Persiana{
 
                 anguloPersiana = clamp<float>(anguloPersiana, 0.0, 50.0);
 
+                if(persianaTextureID == 0){
+                persianaTextureID = SOIL_load_OGL_texture("texturas/persian.jpg", SOIL_LOAD_AUTO, SOIL_CREATE_NEW_ID,
+                                         SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_MULTIPLY_ALPHA);
+            }
+            
+                glEnable(GL_TEXTURE_2D);
+                
+                glBindTexture(GL_TEXTURE_2D, persianaTextureID);
+                glEnable(GL_TEXTURE_GEN_S);
+                glEnable(GL_TEXTURE_GEN_T);
+                GLfloat texCoordScaleS = 0.3;
+                GLfloat texCoordScaleT = 0.3;
+                glTexGenf(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+                glTexGenf(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+                GLfloat texGenS[] = {0.0f, texCoordScaleS, 0.0f, 0.0f, 0.0f};
+                GLfloat texGenT[] = {0.0f,  texCoordScaleT, 0.0f, 0.0f};
+                glTexGenfv(GL_S, GL_OBJECT_PLANE, texGenS);
+                glTexGenfv(GL_T, GL_OBJECT_PLANE, texGenT);
+
                 glPushMatrix();
+                    
                     paralelepipedoFechado(alturaSuporte,largura, profundidade, 0,0,0, corSuporte);
                     glTranslatef(larguraLamina, - (altura-alturaSuporte), - profundidade*0.05);
                     
@@ -60,6 +83,13 @@ class Persiana{
                     }
                 glPopMatrix();
             glPopMatrix();
+            glDisable(GL_TEXTURE_GEN_S);
+            glDisable(GL_TEXTURE_GEN_T);
+
+            glDisable(GL_TEXTURE_2D);
+
+            // glFlush();
+            // glutSwapBuffers();
         }
 
         void abrirOuFecharPersiana(){
